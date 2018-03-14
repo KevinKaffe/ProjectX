d5c5fced56a7898834e90990215c84d5251e3c7f
Kewin <kevikris@gmail.com>
2018-03-14 11:06:59 +0100

    Basic setup of SQLConnector

--- src/SQLConnector.java
+++ src/SQLConnector.java
@@ -1,4 +1,69 @@
+import java.io.IOException;
+import java.sql.Connection;
+
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class SQLConnector {
+	private static String url = "jdbc:mysql://mysql.stud.ntnu.no/kevinkr_project_x";
+	private static String username = "kevinkr_project_x";
+	private static String password = "1234";
+	private static Connection connection;
+	
+	// Metode for aa hente en SQL connection
+	public static Connection getConnection() throws SQLException {
+		if(connection == null) {
+			try {
+				System.out.println("Connecting database...");
+				connection = DriverManager.getConnection(url, username, password);
+				System.out.println("Successfully connected to the database");
+			}
+			catch(SQLException e) {
+				System.out.println("Could not connect to database");
+				throw e;
+			}
+		}
+		return connection;
+	}
+	
+	// Metode for aa lukke en SQL connection
+	public static void closeConnection() {
+		if(connection != null) {
+			try {
+				connection.close();
+				System.out.println("Connection closed");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
+	// Metode for aa hente ut resultatet fra en SQL spoerring
+	public static ResultSet getResultSet(String query) throws SQLException {
+		Connection connection;
+		try {
+			connection = SQLConnector.getConnection();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+			throw e1;
+		}
+		Statement stmt;
+		try {
+			stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			return rs;
+		} catch(SQLException e) {
+			throw e;
+		}
+	}
+	
 }
+
